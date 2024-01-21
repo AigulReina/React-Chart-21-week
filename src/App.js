ea9f7d0f8f621d02196f0f558250631f21a5980f
AigulReina <aigulhanbetova@gmail.com>
2024-01-22 00:28:35 +0500

    change chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Chart } from 'react-google-charts';
 
 function App() {
+  const data = [
+    ['Anime', 'Watching'],
+    ['The Pharmacist Monologue', 20],
+    ['Mashle: Magic and Muscles', 30],
+    ['Spy Family', 50],
+    ['Jujutsu Kaisen', 80],
+  ];
+
+  const options = {
+    title: 'My Top Anime This Month',
+    pieHole: 0, // Регулировка размера отверстия в центре круга (от 0 до 1)
+    pieSliceText: 'percentage', // Отображает проценты на срезах
+    slices: {
+      0: { color: 'orange' }, // Цвет первого среза
+      1: { color: 'green' }, // Цвет второго среза
+      2: { color: 'blue' }, // Цвет третьего среза
+      3: { color: 'purple' },
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Google Charts Example</h1>
+      <Chart
+        chartType="PieChart"
+        data={data}
+        width="100%"
+        height="400px"
+        options={options}
+      />
     </div>
   );
 }
 
 export default App;
+
